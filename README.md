ae4cd5281027f9e884667a57b7dddc2e9c572ac5
ayushyadav07 <ayush.yadav14072003@gmail.com>
2024-09-30 20:13:36 +0530

    project done

---
+++ README.md
@@ -0,0 +1,1 @@
+# AppDevTrack_Project
---
+++ flutter_application_1/lib/utils.dart
@@ -0,0 +1,46 @@
+import 'package:flutter_application_1/firebase_options.dart';
+import 'package:flutter_application_1/services/alert_service.dart';
+import 'package:flutter_application_1/services/auth_service.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_application_1/services/media_service.dart';
+import 'package:flutter_application_1/services/storage_service.dart';
+import 'package:get_it/get_it.dart';
+import 'package:flutter_application_1/services/navigation_service.dart';
+import 'package:flutter_application_1/services/database_service.dart';
+
+Future<void>setupFirebase() async
+{
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+    );
+}
+
+Future<void>registerServices() async
+{
+  final GetIt getIt=GetIt.instance;
+  getIt.registerSingleton<AuthService>(
+    AuthService(),
+  );
+  getIt.registerSingleton<NavigationService>(
+    NavigationService(),
+  );
+  getIt.registerSingleton<AlertService>(
+    AlertService(),
+  );
+  getIt.registerSingleton<MediaService>(
+    MediaService(),
+  );
+  getIt.registerSingleton<StorageService>(
+    StorageService(),
+  );
+  getIt.registerSingleton<DatabaseService>(
+    DatabaseService(),
+  );
+}
+
+String generateChatID({required String uid1, required String uid2}){
+  List uids=[uid1,uid2];
+  uids.sort();
+  String chatID =uids.fold("", (id, uid)=> "$id$uid");
+  return chatID;
+}
